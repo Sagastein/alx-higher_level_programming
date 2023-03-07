2d7734df456f9213a1bc7d3de39bc1a88a7f50de
Sagastein <ishimwe.sage.3@gmail.com>
2023-03-07 05:29:27 -0800

    negative sign

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,12 +2,10 @@
 import random
 number = random.randint(-10000, 10000)
 # YOUR CODE HERE Sagacode
-
 lastdigit = int(str(number)[-1])
-
 if lastdigit > 5:
     print("Last digit of "+ str(number) +" is "+ str(lastdigit) +" and is greater than 5")
 elif lastdigit == 0:
     print("Last digit of "+ str(number) +" is "+ str(lastdigit) +" and is 0")
 else:
-    print("Last digit of "+ str(number) +" is "+ str(lastdigit) +" and is less than 6 and not 0")
+    print("Last digit of "+ str(number) +" is -"+ str(lastdigit) +" and is less than 6 and not 0")
