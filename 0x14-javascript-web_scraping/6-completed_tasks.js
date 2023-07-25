52acbbc6e48eb8ffa3383b3de8a15ced860ed854
Sagastein <ishimwe.sage.3@gmail.com>
2023-07-25 11:50:21 -0700

    fix again

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,16 +1,37 @@
 #!/usr/bin/node
+
 const request = require('request');
-request(process.argv[2], function (error, response, body) {
-  if (!error) {
-    const todos = JSON.parse(body);
-    const completed = {};
-    todos.forEach((todo) => {
-      if (todo.completed && completed[todo.userId] === undefined) {
-        completed[todo.userId] = 1;
-      } else if (todo.completed) {
-        completed[todo.userId] += 1;
-      }
-    });
-    console.log(completed);
-  }
-});
+
+function countCompletedTasks(apiUrl) {
+  request(apiUrl, (error, response, body) => {
+    if (error) {
+      console.error('Error:', error);
+    } else if (response.statusCode !== 200) {
+      console.error('Status:', response.statusCode);
+    } else {
+      const todos = JSON.parse(body);
+      const completedTasksByUserId = {};
+
+      todos.forEach((todo) => {
+        if (todo.completed) {
+          if (completedTasksByUserId[todo.userId]) {
+            completedTasksByUserId[todo.userId]++;
+          } else {
+            completedTasksByUserId[todo.userId] = 1;
+          }
+        }
+      });
+
+      console.log(completedTasksByUserId);
+    }
+  });
+}
+
+const apiUrl = process.argv[2];
+
+if (!apiUrl) {
+  console.error('Usage: ./6-completed_tasks.js <API_URL>');
+  process.exit(1);
+}
+
+countCompletedTasks(apiUrl);
